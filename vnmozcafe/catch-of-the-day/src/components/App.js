c5b2a085ab832ff30e0a43aa54a98709b6b59019
leolee2708 <leho.vietquan@gmail.com>
2021-03-09 20:16:48 -0500

    added menu for the farmer market

--- vnmozcafe/catch-of-the-day/src/components/App.js
+++ vnmozcafe/catch-of-the-day/src/components/App.js
@@ -13,10 +13,25 @@ class App extends React.Component {
   };
   componentDidMount() {
     const { params } = this.props.match;
-    this.ref = base.syncState(`${params.storeId}/fishes`,{
-        comtext:this,
-        state:'fishes'
+
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) })
+    };
+
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
     });
+  }
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+  componentWillUnmout() {
+    base.removeBinding(this.ref);
   }
   addFish = (fish) => {
     //copy of the existing state
@@ -31,6 +46,15 @@ class App extends React.Component {
       fishes,
     });
   };
+
+  updateFish = (key, updatedFish) => {
+    const fishes = { ...this.state.fishes };
+
+    fishes[key] = updatedFish;
+
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -59,7 +83,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
